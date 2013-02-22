a4e9296c9a73ea82edcffcc4542ab693ead303f4
Pavel Minsky <pavelminsky@gmail.com>
2013-02-22 23:44:26 +0400

    crappy temp - "done"

--- index.php
+++ index.php
@@ -64,35 +64,85 @@ video/lastexorcism2_russiantrailer_550.ogv
 -->
 
 
+<style type="text/css">
+html {height: 100%;}
+body {overflow: auto; height: 100%; min-width: 960px; min-height: 600px; background: #f9f9f9 url('/images/body_bg.png') left top repeat; color: #393939; line-height: 14px; font-family: 'Playfair Display SC', 'Garamond', serif;}
 
-<p>&nbsp;</p>
+.age {position: absolute; top: 10px; left: 10px; width: 50px; height: 50px; z-index: 2; background: url('/images/age.png') center center no-repeat;}
+.puzzle {position: absolute; top: 10px; right: 10px; z-index: 2;}
+.puzzle a {display: block; width: 50px; height: 50px; text-decoration: none; background: url('/images/puzzle.png') center center no-repeat;}
 
-<div id="lastexorcism2_russiantrailer_550" style="width: 800px; height: 332px;;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="332" poster="/video/lastexorcism2_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.ogv" type='video/ogg'></video></div>
+.superwrap {width: 880px; margin: 0 auto; text-align: center; font-size: 80%;}
 
-<p>&nbsp;</p>
+.supertop {width: 800px; height: 281px; margin: 0 auto; background: url('/images/supertop.png') center center no-repeat;}
 
-<p><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/01.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/02.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/03.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/04.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/05.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/06.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/07.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/08.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/08s.jpg" alt=""></a></p>
+.supercontent {width: 870px; margin: 0 auto; padding: 20px 0 20px 0; text-align: center;}
 
-<h1><a href="/puzzle/">Puzzle</a></h1>
+.supercontent .superleft {float: left; width: 420px; height: 500px; padding-right: 10px; background: url('/images/superright_bg.png') center center no-repeat;}
+.supercontent .superright {float: right; width: 420px; margin: 30px 0 20px 10px; font-size: 14px; line-height: 20px; text-align: left; text-shadow: 0 1px 1px #ffffff;}
+.supercontent .superright p {padding-bottom: 14px;}
+.supercontent .superright b {font-weight: bold;}
+
+a:link, a:visited {color: #624d24;}
+a:hover {color: #9c683a;}
+
+#counters {position: fixed; bottom: -160px; right: 40px; z-index: 1005; width: 140px; height: 180px; text-align: right; background-image:url('/images/counters.png'); background-position: right top; background-repeat: no-repeat;}
+.counters_list {padding-top: 30px; opacity: 0.40; -moz-opacity: 0.40; -khtml-opacity: 0.40; filter: alpha(opacity=40);}
+.counters_list p {padding: 0; margin: 0; padding-bottom: 6px; font-size: 11px; color: #3e4548;}
+#counters:hover {bottom: 0;}
+</style>
 
 
+<div class="age">&nbsp;</div>
+<div class="puzzle"><a href="/puzzle/">&nbsp;</a></div>
+
+<div class="superwrap">
+
+	<div class="supertop">&nbsp;</div>
+
+	<p>&nbsp;</p>
+
+	<div id="lastexorcism2_russiantrailer_550" style="width: 800px; height: 332px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="332" poster="/video/lastexorcism2_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.lastexorcism2.ru/video/lastexorcism2_russiantrailer_550.ogv" type='video/ogg'></video></div>
 
 
-<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
-
-
-
-<div id="counters">
-	<div class="counters_list">
-	<p>Сюда изгнаны счетчики:</p>
-	<p>
-	<?php include ("counters_include.php"); ?>	
-	</p>
+	<div class="supercontent clearfix">
+		<div class="superleft">&nbsp;</div>
+		<div class="superright">
+			<p><b>Нелл находят в лесу</b> — грязную и напуганную, только что сбежавшую со страшного ритуала. Вся ее семья погибла и девушку отправляют в Новый Орлеан — приют для девочек. Там она попадает под присмотр терапевта Фрэнка Мерле. Жизнь вроде бы налаживается, Нелл влюбляется в местного мальчика, но в самый неподходящий момент демон вновь объявляется и овладевает Нелл. Теперь он хочет от нее нечто большее…</p>
+			<p style="color: #9d1e00;">Первый фильм вышел в прокат в 2010 году.</p>
+			<p style="color: #9d1e00;"><b>Второе пришествие Зла на российские экраны состоится 28 февраля 2013 года.</b></p>
+			<p>&nbsp;</p>
+			<p><b>Режиссер:</b> Эд Гасс-Доннели</p>
+			<p><b>Сценарий:</b> Дэмьен Шазелл, Эд Гасс-Доннели</p>
+			<p><b>В ролях:</b> Спенсер Трит Кларк, Эшли Белл, Эндрю Сенсениг, Джудд Лорманд, Джулия Гарнер, Мьюз Уотсон, Э. Роджер Митчелл</p>
+		</div>
 	</div>
+
+
+	<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+
+	<? //<p><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/01.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/02.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/03.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/04.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/05.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/06.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/07.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="http://www.beautifulcreaturesmovie.ru/gallery/stills/08.jpg"><img src="http://www.beautifulcreaturesmovie.ru/gallery/stills/08s.jpg" alt=""></a></p> ?>
+
+	<!--h1><a href="/puzzle/">Puzzle</a></h1-->
+
+
+
+
+	<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
+
+
+
+	<div id="counters">
+		<div class="counters_list">
+		<p>Сюда изгнаны счетчики:</p>
+		<p>
+		<?php include ("counters_include.php"); ?>	
+		</p>
+		</div>
+	</div>
+
+
 </div>
-
-
-
 
 
 
